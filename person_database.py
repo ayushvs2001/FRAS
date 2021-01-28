61668821adbe0e2015ef70bd45c4758e3d080026
ayushvs2001 <77103709+ayushvs2001@users.noreply.github.com>
2021-01-28 13:24:59 +0530

    third commit

--- person_database.py
+++ person_database.py
@@ -98,31 +98,58 @@ def mark_attendance():
     conn.close()
 
     cap = cv2.VideoCapture(0)
+
+    thres = 0.45  # Threshold to detect object
+    configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'  # for object detection such as cellphone
+    weightsPath = 'frozen_inference_graph.pb'
+
+    net = cv2.dnn_DetectionModel(weightsPath, configPath)
+    net.setInputSize(320, 320)
+    net.setInputScale(1.0 / 127.5)
+    net.setInputMean((127.5, 127.5, 127.5))
+    net.setInputSwapRB(True)
+
     while True:
         success, img = cap.read()
-        imgs = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-        imgs = cv2.cvtColor(imgs, cv2.COLOR_BGR2RGB)
 
-        facesCurFrame = face_recognition.face_locations(imgs)  # sending only first img
-        encodeCurFrame = face_recognition.face_encodings(imgs, facesCurFrame)
+        # for object detection
+        classIds, confs, bbox = net.detect(img, confThreshold=thres)
+        list_of_object = {77: "cell phone", 73: "laptop"}
+        temp = img
+        confidence_of_required_object = 0
 
-        for encodeFace, faceLoc in zip(encodeCurFrame, facesCurFrame):
-            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-            # print(faceDis)
-            matchIndex = numpy.argmin(faceDis)
+        if len(classIds) != 0:
+            for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+                if classId in list_of_object.keys():
+                    cv2.rectangle(temp, box, color=(0, 255, 0), thickness=2)
+                    confidence_of_required_object = int(confidence * 100)
 
-            if matches[matchIndex]:
-                name = classNames[matchIndex].upper()
-                # print(name)
-                y1, x2, y2, x1 = faceLoc
-                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-                if name not in list_of_names:
-                   list_of_names.add(name)
-                   add_attedance(name)
+       # if object not detected then process go on for face recognition
+        if confidence_of_required_object < 48:
+
+            imgs = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+            imgs = cv2.cvtColor(imgs, cv2.COLOR_BGR2RGB)
+
+            facesCurFrame = face_recognition.face_locations(imgs)  # sending only first img
+            encodeCurFrame = face_recognition.face_encodings(imgs, facesCurFrame)
+
+            for encodeFace, faceLoc in zip(encodeCurFrame, facesCurFrame):
+                matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+                faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+                # print(faceDis)
+                matchIndex = numpy.argmin(faceDis)
+
+                if matches[matchIndex]:
+                    name = classNames[matchIndex].upper()
+                    # print(name)
+                    y1, x2, y2, x1 = faceLoc
+                    y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                    cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                    if name not in list_of_names:
+                       list_of_names.add(name)
+                       add_attedance(name)
 
         cv2.imshow('webcam', img)
 
